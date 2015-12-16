036012d7ca0cb6563ccef3ff0b6f69dbd88bdcb7
freewomancode <stefficano@gmail.com>
2015-12-16 01:54:01 -0600

    Use method to apply the rules and include css to show it

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var size = 20;
+var size = 40;
 
 var x = 0;
 var y = 0;
@@ -15,9 +15,9 @@ function drawUniverse(){
     universe.forEach(function(row){
       var tr = document.createElement('tr');
       row.forEach(function(column){
+      var clase = column == 1 ? "alive" : "death";
       var td = document.createElement('td');
-      td.id = 'tdId';
-      td.appendChild(document.createTextNode(column));
+      td.setAttribute('class', clase);
       tr.appendChild(td);
     });
 
@@ -25,30 +25,49 @@ function drawUniverse(){
   });
 
   table.appendChild(tbody);
-  document.body.innerHTML = "";
-  document.body.appendChild(table);
-}
-window.onload = function(){
-  drawUniverse();
+  var container = document.querySelector('.tableGame');
+  container.innerHTML = "";
+  container.appendChild(table);
 }
 
-function run() {
-  for (i = 0; i < universe.length; i++) {
-    for (j = 0; j < universe.length; j++) {
+function initialize() {
+  for (var i = 0; i < universe.length; i++) {
+    for (var j = 0; j < universe.length; j++) {
       if (Math.random() > 0.6) {
         universe[i][j] = 1;
       } else {
-        universe[i][j] = "";
+        universe[i][j] = 0;
       }
-      close(i, j);
     }
   }
 }
 
-function close() {
-  var neighbors = (x, y) + (x + 1, y + 1) + (x+1, y) + (x-1, y - 1) + (x - 1, y + 1)
-  + (x - 1) + (y - 1, x) + (y + 1, x) + (x + 1, y - 1);
+window.onload = function(){
+  initialize();
+  drawUniverse();
+}
+
+function run() {
+  for (var i = 0; i < universe.length; i++) {
+    for (var j = 0; j < universe.length; j++) {
+      rules(universe, i, j);
+    }
+  }
+}
+
+function close(x, y) {
+  var neighbors = getValue(x, y) + getValue(x + 1, y + 1) + getValue(x+1, y) +
+  getValue(x-1, y - 1) + getValue(x - 1, y + 1) + getValue(x - 1) + getValue(y - 1, x)
+  + getValue(y + 1, x) + getValue(x + 1, y - 1);
   return neighbors;
+}
+
+function getValue(x, y) {
+  try {
+    return isNaN(universe[x][y]) ? 0 : universe[x][y];
+  } catch(error) {
+    return 0;
+  }
 }
 
 function rules(universe, i, j){
@@ -69,7 +88,7 @@ function rules(universe, i, j){
     if (neighbors == 3) {
       universe[i][j] = 1;
     }
-}
+  }
 
 setInterval(function(){
   run();
